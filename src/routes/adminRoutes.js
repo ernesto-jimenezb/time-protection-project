95381a95eb9fdfe5db504cc3873d13304b5bad3f
IsaacPG11 <137459133+Isaac11PG@users.noreply.github.com>
2025-12-18 12:37:37 -0600

    Feature: Estructura base para rutas de administrador

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,3 +1,14 @@
 import express from "express";
 const router = express.Router();
+
+// Ruta simple de ejemplo (GET)
+router.get("/", (req, res) => {
+    res.send("¡El servidor está funcionando correctamente!");
+});
+
+// Ejemplo de otra ruta para una página de inicio de sesión
+router.get("/login", (req, res) => {
+    res.render("login"); // Esto buscará el archivo login.ejs en tu carpeta de vistas
+});
+
 export default router;
